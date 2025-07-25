1e3dcbf5ab162313311e672bd3110653e273619a
taghajan <11960+taghajan@users.noreply.cto-github.cisco.com>
2025-07-25 20:46:05 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'wedding-invite-app';
+export class AppComponent implements OnInit {
+  audio!: HTMLAudioElement;
+  isPlaying = false;
+
+  ngOnInit(): void {
+    this.audio = document.getElementById('bg-music') as HTMLAudioElement;
+    this.audio.loop = true;
+
+    // Start muted so autoplay works
+    this.audio.muted = true;
+
+    // Try to play muted audio automatically
+    this.audio.play().then(() => {
+      this.isPlaying = true; 
+    }).catch(() => {
+      this.isPlaying = false;
+    });
+  }
+
+  toggleAudio() {
+    if (this.isPlaying) {
+      this.audio.pause();
+      this.isPlaying = false;
+    } else {
+      // On user interaction unmute and play
+      this.audio.muted = false;
+      this.audio.play();
+      this.isPlaying = true;
+    }
+  }
 }
